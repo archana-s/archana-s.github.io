bf0f6fdf977ae743dc839ffb2401a603c76eb9a0
Archana S <archana.june@gmail.com>
2014-03-13 18:18:38 -0700

    Added the floating shapes and rearranged files

---
+++ floatingShapes/script.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+   var stage = new Kinetic.Stage({
+        container: 'workarea',
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+
+    var layer = new Kinetic.Layer();
+    var allCircles = [];
+    var listOfPositions = [];
+    var listOfMotionFunctions = [];
+    var NUM_CIRCLES = 50;
+
+    for (var i=0; i < NUM_CIRCLES; i++) {
+
+      var pos = getRandomLocation(stage.getWidth()/2, stage.getHeight()/2);
+      var circle = new Kinetic.Circle({
+        x: pos.x,
+        y: pos.y,
+        radius: getRandomRadius(1,4),
+        fill: getRandomColor()
+      });
+
+      // add the shape to the layer
+      layer.add(circle);
+
+      // add the layer to the stage
+      stage.add(layer);
+      allCircles.push(circle);
+      listOfPositions.push(getRandomLocation(stage.getWidth(), stage.getHeight()));
+      listOfMotionFunctions.push({xFunc: getRandomMotionFunction(), yFunc: getRandomMotionFunction()});
+    }
+
+    // Add animations
+    var velocity = 150;
+    var period = 25000;
+
+    var anim = new Kinetic.Animation(function(frame) {
+      for (var i=0; i<allCircles.length; i++) {
+        var pos = listOfPositions[i];
+        var motions = listOfMotionFunctions[i];
+
+        xVal = motions.xFunc(velocity, frame.time, period) + pos.x;
+        yVal = motions.yFunc(velocity, frame.time, period) + pos.y;
+
+        allCircles[i].setX(xVal);
+        allCircles[i].setY(yVal);
+      }
+    }, layer);
+    anim.start();
+});
+
+var getRandomMotionFunction = function() {
+  var val = Math.round(Math.random() * 3);
+  switch(val) {
+    case 0:
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(frameTime * 2 * (Math.PI / period))
+              };
+      break;
+    case 1:
+      return function(velocity, frameTime, period) {
+                return velocity * Math.cos(frameTime * 2 * (Math.PI / period))
+              };
+      break;
+    case 2:
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(-1 * frameTime * 2 * (Math.PI / period))
+              };
+      break;
+    case 3:
+      return function(velocity, frameTime, period) {
+                return velocity * Math.cos(-1 * frameTime * 2 * (Math.PI / period))
+              };
+      break;
+    default:
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(frameTime * 2 * (Math.PI / period))
+              };
+      break;
+  };
+};
+
+var getRandomLocation =  function(width, height) {
+  var x = Math.round(Math.random() * width);
+  var y = Math.round(Math.random() * height);
+  return {x: x, y: y};
+};
+
+var getRandomColor = function() {
+  var colors = ["rgb(122, 241, 251)", "rgb(255, 223, 0)", "rgb(189, 153, 167)", "rgb(231, 95, 28)", "rgb(92, 87, 84)", "rgb(245, 236, 216)", "rgb(37, 172, 182)",
+    "rgb(234, 75, 88)", "rgb(146, 120, 106)", "rgb(84, 49, 80)", "rgb(101, 142, 109)", "rgb()246, 168, 121"
+  ];
+  var num = Math.round(Math.random() * colors.length);
+  return colors[num-1];
+};
+
+var getRandomRadius = function(max, min) {
+   return Math.round(Math.random() * (max - min) + min);
+};
